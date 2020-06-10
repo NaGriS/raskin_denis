41bc5aeb3d93bc5703e2436235a5fbe452f2ab3d
NaGriS <rsdenissr@gmail.com>
2020-06-10 11:58:06 +0300

    Created a couple of examples how to work with WinRegistry

---
+++ RegStuff/RegStuff/SettingsList.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace RegStuff
+{
+    public class SettingsList
+    {
+        [JsonProperty("ListOfSettings")]
+        public List<Setting> ListOfSettings { get; set; }
+
+        public SettingsList()
+        {
+        }
+
+        public SettingsList(List<Setting> settingsList)
+        {
+            ListOfSettings = settingsList;
+        }
+    }
+}
